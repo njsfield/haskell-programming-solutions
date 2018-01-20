5591e12612927e2e8c349d3c8455f396759d6df3
Nick Field <heyimnick@live.com>
2018-01-20 14:08:34 +0000

    add notes to Compose

--- src/ch25/Compose.hs
+++ src/ch25/Compose.hs
@@ -12,7 +12,7 @@ newtype Compose f g a =
 {- Functor (fmap)
   (.)  : (x -> y) -> (v -> x) -> (v -> y)
   fmap : (a -> b) -> (f a -> f b)
-  ((.) fmap famp) :  ((f a -> f b) -> (t (f a) -> t (f b))
+  ((.) fmap fmap) :  ((f a -> f b) -> (t (f a) -> t (f b))
                   -> (a -> b) -> (f a -> f b)
                   -> (a -> b) -> (t (f a) -> t (f b))
 -}
@@ -22,7 +22,7 @@ instance (Functor f, Functor g) =>
   fmap f (Compose fga) =
     Compose $ (fmap . fmap) f fga
 
-{- Aapplicative
+{- Applicative
   (<*>) : t (x -> y) -> t x -> t y
   fmap (<*>) : f (t (x -> y)) -> f (t a -> t b)
   fmap (<*>) fgab : f (t a -> t b)
@@ -40,10 +40,27 @@ instance (Applicative f, Applicative g) =>
   (Compose f) <*> (Compose a) =
     Compose $ (fmap (<*>) f) <*> a
 
+{- Foldable
+   foldMap : (a -> m) -> g a -> m
+   f : (a -> m)
+   a : (f (g a))
+   foldMap f : g a -> m
+   foldMap (foldMap f) : f (g a) -> m
+-}
+
+
 instance (Foldable f, Foldable g) =>
           Foldable (Compose f g) where
   foldMap f (Compose a) = foldMap (foldMap f) a
 
+
+{- Traversable
+   traverse : (a -> f b) -> g a -> f (g b)
+   f : (a -> f b)
+   a : (f (g a))
+   traverse f : g a -> f (g b)
+   traverse (traverse f) : f (g a) -> h (f (g b))
+-}
 instance (Traversable f, Traversable g) =>
           Traversable (Compose f g) where
-  traverse f (Compose a) = Compose <$> traverse (traverse f) a
+  traverse f (Compose a) = (Compose <$> traverse (traverse f)) a
