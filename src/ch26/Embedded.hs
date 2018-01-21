9954fd5e9f8216bbd429055810d87bc427f4f0fa
Nick Field <heyimnick@live.com>
2018-01-21 09:53:15 +0000

    add LiftIO instances

---
+++ src/ch26/Embedded.hs
@@ -0,0 +1,8 @@
+module Ch26.Embedded where
+
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Reader
+
+embedded :: MaybeT (ExceptT String (ReaderT () IO)) Int
+embedded = MaybeT . ExceptT . ReaderT $ const(return (Right (Just 1)))
